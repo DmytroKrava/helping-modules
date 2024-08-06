fc6691d08ad5624cb46fa0a920b690f203391a91
DmytroKrava <93785602+DmytroKrava@users.noreply.github.com>
2024-08-06 12:22:00 +0300

    Update interactive-hints.js

--- interactive-hints/interactive-hints.js
+++ interactive-hints/interactive-hints.js
@@ -2,7 +2,7 @@ import './style.css';
 
 
 function setDisplayForElement(element, value) {
-	let allElements = document.getElementsByClassName(element);
+    let allElements = document.getElementsByClassName(element);
     for (let i = 0; i < allElements.length;	i++) {
        	allElements[i].style.display = value;
     }
@@ -10,16 +10,16 @@ function setDisplayForElement(element, value) {
 
 
 export function changeRegime(checkBoxValue) {
-	if (!checkBoxValue) {
-		setDisplayForElement('fas fa-play', 'none');
-		setDisplayForElement('fas fa-stop', 'none');
-		setDisplayForElement('modal-window-button', 'none');
-		setDisplayForElement('show-help', 'none');
+    if (!checkBoxValue) {
+	setDisplayForElement('fas fa-play', 'none');
+	setDisplayForElement('fas fa-stop', 'none');
+	setDisplayForElement('modal-window-button', 'none');
+	setDisplayForElement('show-help', 'none');
     } else {
     	setDisplayForElement('fas fa-play', 'block');
-		setDisplayForElement('fas fa-stop', 'block');
-		setDisplayForElement('modal-window-button', 'block');
-		setDisplayForElement('show-help', 'block');
+	setDisplayForElement('fas fa-stop', 'block');
+	setDisplayForElement('modal-window-button', 'block');
+	setDisplayForElement('show-help', 'block');
     }
 }
 
@@ -29,7 +29,7 @@ let titleCopy = "";
 
 
 function outFromMouse() {
-	const tooltip = document.querySelector(".tooltip-style");
+    const tooltip = document.querySelector(".tooltip-style");
     if (tooltip) {
       	hovered = false;
       	document.body.removeChild(tooltip);
@@ -52,31 +52,31 @@ export function tooltip(event) {
     let buttonRect = button.getBoundingClientRect();
     let tooltipRect = tooltip.getBoundingClientRect();
 
-	if (buttonRect.bottom + tooltipRect.height < window.innerHeight) {
-		tooltip.style.top = buttonRect.bottom + window.pageYOffset + 'px';
-	}
-	else {
-		tooltip.style.top = buttonRect.top + window.pageYOffset - tooltipRect.height + 'px';
-	}
+    if (buttonRect.bottom + tooltipRect.height < window.innerHeight) {
+	tooltip.style.top = buttonRect.bottom + window.pageYOffset + 'px';
+    }
+    else {
+	tooltip.style.top = buttonRect.top + window.pageYOffset - tooltipRect.height + 'px';
+    }
 
-	if (buttonRect.left + tooltipRect.width < window.innerWidth) {
-		tooltip.style.left = buttonRect.left + 'px';
-	}
-	else {
-		tooltip.style.left = buttonRect.left + buttonRect.width - tooltipRect.width + 'px';
-	}
+    if (buttonRect.left + tooltipRect.width < window.innerWidth) {
+	tooltip.style.left = buttonRect.left + 'px';
+    }
+    else {
+	tooltip.style.left = buttonRect.left + buttonRect.width - tooltipRect.width + 'px';
+    }
 
     button.addEventListener("mouseout", outFromMouse);
 }
 
 
 function dropModalWindow(event) {
-	let close = document.querySelector(".close-modal");
+    let close = document.querySelector(".close-modal");
 
-	if (close.contains(event.target)) {
-		const tooltip = document.querySelector(".modal-window");
-	    document.body.removeChild(tooltip);
-	}
+    if (close.contains(event.target)) {
+	const tooltip = document.querySelector(".modal-window");
+	document.body.removeChild(tooltip);
+    }
 }
 
 
@@ -109,30 +109,30 @@ export function modalWindow(event) {
 
 
 export function audio(name) {
-	const playButton = document.getElementsByName(name)[0];
-	let audio = new Audio(playButton.dataset.arg1);
+    const playButton = document.getElementsByName(name)[0];
+    let audio = new Audio(playButton.dataset.arg1);
 
-	playButton.addEventListener('click', function() {
-	  if (audio.paused) {
+    playButton.addEventListener('click', function() {
+	if (audio.paused) {
 	    playButton.classList.remove('fa-play');
 	    playButton.classList.toggle('fa-stop');
 	    audio.load();
 	    audio.play();
-	  }
-	  else {
-		  playButton.classList.remove('fa-stop');
-		  playButton.classList.toggle('fa-play');
-		  audio.pause();
-	  }
-	});
+	}
+	else {
+	    playButton.classList.remove('fa-stop');
+	    playButton.classList.toggle('fa-play');
+	    audio.pause();
+	}
+    });
 
-	document.addEventListener('keydown', function(event) {
-		event.preventDefault();
-	});
+    document.addEventListener('keydown', function(event) {
+	event.preventDefault();
+    });
 
-	audio.addEventListener('ended', function() {
-		playButton.classList.remove('fa-stop');
-		playButton.classList.toggle('fa-play');
-		audio.load();
-	});
+    audio.addEventListener('ended', function() {
+	playButton.classList.remove('fa-stop');
+	playButton.classList.toggle('fa-play');
+	audio.load();
+    });
 }
